509ca2d3bd2bf37bceedd481c8bd3b6d93302612
FlyBenja <aldanabenjamin185@gmail.com>
2024-10-30 13:45:27 -0600

    Mejoramiendo a la tabla de roles para la creacion

--- models/role.js
+++ models/role.js
@@ -1,21 +1,18 @@
-// models/role.js
-
 module.exports = (sequelize, DataTypes) => {
-    const Role = sequelize.define('Role', {
-      nombreRole: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-      },
+  const Role = sequelize.define('Role', {
+    nombreRole: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+  });
+
+  Role.associate = (models) => {
+    Role.hasMany(models.User, {
+      foreignKey: 'roleId',
+      as: 'users',
     });
-  
-    Role.associate = (models) => {
-      Role.hasMany(models.User, {
-        foreignKey: 'roleId',
-        as: 'users',
-      });
-    };
-  
-    return Role;
   };
-  
+
+  return Role;
+};
