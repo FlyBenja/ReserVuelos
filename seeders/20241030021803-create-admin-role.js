47fe7c15f26e7049fef13961af8045f769990bdd
FlyBenja <aldanabenjamin185@gmail.com>
2024-10-30 13:38:50 -0600

    Creacion de seeders

--- seeders/20241030021803-create-admin-role.js
+++ seeders/20241030021803-create-admin-role.js
@@ -2,16 +2,22 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Roles', [
-      {
-        nombreRole: 'Admin',  // Nombre del rol
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ], {});
+    const [results] = await queryInterface.sequelize.query(
+      "SELECT * FROM Roles WHERE nombreRole = 'Admin'"
+    );
+
+    if (results.length === 0) {
+      await queryInterface.bulkInsert('Roles', [
+        {
+          nombreRole: 'Admin',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ], {});
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('Roles', { nombreRole: 'Admin' }, {});  // Eliminar solo el rol "Admin"
+    await queryInterface.bulkDelete('Roles', { nombreRole: 'Admin' }, {});
   }
 };
--- seeders/20241030070753-create-admin-user.js
+++ seeders/20241030070753-create-admin-user.js
@@ -3,15 +3,23 @@ const bcrypt = require('bcrypt');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const hashedPassword = await bcrypt.hash('admin', 10); // Hashea la contraseña
+    // Verificar si el rol Admin existe
+    const [results] = await queryInterface.sequelize.query(
+      "SELECT * FROM Roles WHERE nombreRole = 'Admin'"
+    );
 
-    await queryInterface.bulkInsert('Users', [{
-      username: 'admin',
-      password: hashedPassword,
-      roleId: 1, // Asegúrate de que el rol con ID 1 existe
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }], {});
+    // Solo insertar el usuario si el rol Admin existe
+    if (results.length > 0) {
+      const hashedPassword = await bcrypt.hash('admin', 10); // Hashea la contraseña
+
+      await queryInterface.bulkInsert('Users', [{
+        username: 'admin',
+        password: hashedPassword,
+        roleId: results[0].id, // Usar el ID del rol encontrado
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }], {});
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
