a8eafac6de9c0b91c657728c46fccbb0906d2619
FlyBenja <aldanabenjamin185@gmail.com>
2024-10-30 16:42:01 -0600

    mejorando el apartado de seeder de usuario v4

--- seeders/20241030070753-create-admin-user.js
+++ seeders/20241030070753-create-admin-user.js
@@ -3,33 +3,18 @@ const bcrypt = require('bcrypt');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    // Verificar si la tabla 'Roles' existe
-    const tableExists = await queryInterface.sequelize.query(
-      "SELECT to_regclass('public.Roles')"
-    );
+    // Hashear la contraseña
+    const hashedPassword = await bcrypt.hash('admin', 10); // Hashea la contraseña 'admin'
 
-    if (tableExists[0][0].to_regclass) {
-      // Asegurarse de que el rol con role_id = 1 existe
-      const [results] = await queryInterface.sequelize.query(
-        "SELECT * FROM Roles WHERE id = 1"
-      );
+    // Insertar el usuario admin con roleId = 1
+    await queryInterface.bulkInsert('Users', [{
+      username: 'admin',
+      password: hashedPassword,
+      roleId: 1, // Asignar roleId = 1 fijo
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
 
-      // Solo insertar el usuario si el rol con role_id = 1 existe
-      if (results.length > 0) {
-        const hashedPassword = await bcrypt.hash('admin', 10); // Hashea la contraseña
-
-        await queryInterface.bulkInsert('Users', [{
-          username: 'admin',
-          password: hashedPassword,
-          roleId: 1, // Asignar role_id = 1 fijo
-          createdAt: new Date(),
-          updatedAt: new Date()
-        }]);
-      } else {
-        console.warn("El rol con role_id = 1 no existe. No se creará el usuario.");
-      }
-    } else {
-      console.warn("La tabla 'Roles' no existe. Asegúrate de ejecutar las migraciones primero.");
-    }
-  },
+    console.log("Usuario 'admin' creado exitosamente.");
+  }
 };
