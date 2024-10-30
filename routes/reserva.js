6efdb748e4d6be2996cd29cdd98dabcbc68080a3
FlyBenja <aldanabenjamin185@gmail.com>
2024-10-30 17:59:56 -0600

    Empezando la creacion de reserva individual v2

--- routes/reserva.js
+++ routes/reserva.js
@@ -16,9 +16,29 @@ const reservaController = require('../controllers/reservaController');
  *   post:
  *     summary: Crea una nueva reserva sin pasajeros
  *     tags: [Reservaciones]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               codigoReserva:
+ *                 type: string
+ *                 example: "RSV12345"
+ *               fechaInicio:
+ *                 type: string
+ *                 format: date
+ *                 example: "2024-10-28"
+ *               fechaFinal:
+ *                 type: string
+ *                 format: date
+ *                 example: "2024-11-02"
  *     responses:
  *       201:
  *         description: Reserva creada exitosamente
+ *       400:
+ *         description: Error.- Código de reserva ya existe
  *       500:
  *         description: Error al crear la reserva
  */
