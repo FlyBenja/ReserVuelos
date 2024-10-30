9d23bee89fd69adf3e16b470c82e2038f292608e
FlyBenja <aldanabenjamin185@gmail.com>
2024-10-30 08:12:19 -0600

    Corrigiendo CORS

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocs = require('./swaggerConfig');  // Importa la configuración de Swagger
+const swaggerDocs = require('./swaggerConfig'); // Configuración de Swagger
+const cors = require("cors"); // Importa el módulo CORS
 const db = require('./models');
 const reservaRoutes = require('./routes/reserva');
 const claseVueloRoutes = require('./routes/claseVuelo');
@@ -11,19 +12,27 @@ const roleRoutes = require('./routes/role');
 const authenticateToken = require('./Middleware/authenticateToken');
 
 const app = express();
+
+// Configuración de CORS
+app.use(cors({
+  origin: ["http://localhost:5173", "https://examen-final-wep.netlify.app"], // Dominios permitidos
+  methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
+  allowedHeaders: ["Content-Type", "Authorization"], // Headers permitidos
+}));
+
 app.use(express.json());
 
 // Swagger setup
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Rutas sin autenticación (Users)
-app.use('/api/users', userRoutes); // Ruta sin autenticación para creación y login de usuarios
+app.use('/api/users', userRoutes); // Rutas públicas para creación y login de usuarios
 
 // Rutas protegidas con autenticación y roles específicos
-app.use('/api/roles', authenticateToken([1]), roleRoutes);            // Solo accesible para roleId = 1 (Admin)
-app.use('/api/clases-vuelo', authenticateToken([1]), claseVueloRoutes); // Solo accesible para roleId = 1
-app.use('/api/reservas', authenticateToken([1]), reservaRoutes);       // Solo accesible para roleId = 1
-app.use('/api/pasajeros', authenticateToken([2]), pasajeroRoutes);     // Solo accesible para roleId = 2
+app.use('/api/roles', authenticateToken([1]), roleRoutes);            // Accesible solo para roleId = 1 (Admin)
+app.use('/api/clases-vuelo', authenticateToken([1]), claseVueloRoutes); // Accesible solo para roleId = 1
+app.use('/api/reservas', authenticateToken([1]), reservaRoutes);       // Accesible solo para roleId = 1
+app.use('/api/pasajeros', authenticateToken([2]), pasajeroRoutes);     // Accesible solo para roleId = 2
 
 const PORT = process.env.PORT || 3000;
 db.sequelize.sync().then(() => {
