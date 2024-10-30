ee8d7f37d729dddf3ea4a45d00d2ba72a3198ad0
FlyBenja <aldanabenjamin185@gmail.com>
2024-10-30 11:35:54 -0600

    Verificando seeders para que no cree si ya existe

--- seeders/20241030021803-create-admin-role.js
+++ seeders/20241030021803-create-admin-role.js
@@ -1,17 +1,23 @@
+// seeds/20241030021803-create-admin-role.js
 'use strict';
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Roles', [
-      {
-        nombreRole: 'Admin',  // Nombre del rol
+    const [results, metadata] = await queryInterface.sequelize.query(
+      "SELECT * FROM Roles WHERE nombreRole = 'Admin'"
+    );
+    if (results.length === 0) {
+      await queryInterface.bulkInsert('Roles', [{
+        nombreRole: 'Admin',
         createdAt: new Date(),
         updatedAt: new Date(),
-      },
-    ], {});
+      }]);
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('Roles', { nombreRole: 'Admin' }, {});  // Eliminar solo el rol "Admin"
+    await queryInterface.bulkDelete('Roles', {
+      nombreRole: 'Admin',
+    });
   }
 };
--- seeders/20241030070753-create-admin-user.js
+++ seeders/20241030070753-create-admin-user.js
@@ -3,15 +3,23 @@ const bcrypt = require('bcrypt');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const hashedPassword = await bcrypt.hash('admin', 10); // Hashea la contraseña
+    // Verificar si el usuario ya existe
+    const [results, metadata] = await queryInterface.sequelize.query(
+      "SELECT * FROM Users WHERE username = 'admin'"
+    );
 
-    await queryInterface.bulkInsert('Users', [{
-      username: 'admin',
-      password: hashedPassword,
-      roleId: 1, // Asegúrate de que el rol con ID 1 existe
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }], {});
+    // Solo insertar si el usuario no existe
+    if (results.length === 0) {
+      const hashedPassword = await bcrypt.hash('admin', 10); // Hashea la contraseña
+
+      await queryInterface.bulkInsert('Users', [{
+        username: 'admin',
+        password: hashedPassword,
+        roleId: 1, // Asegúrate de que el rol con ID 1 existe
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }], {});
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
