bfbf335fb6d545d1cf40b6fa0289344d0b4817a4
FlyBenja <aldanabenjamin185@gmail.com>
2024-10-30 01:09:02 -0600

    Creacion de nuevo seeder y modificando el .json para postgress

---
+++ seeders/20241030070753-create-admin-user.js
@@ -0,0 +1,20 @@
+'use strict';
+const bcrypt = require('bcrypt');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const hashedPassword = await bcrypt.hash('admin', 10); // Hashea la contraseña
+
+    await queryInterface.bulkInsert('Users', [{
+      username: 'admin',
+      password: hashedPassword,
+      roleId: 1, // Asegúrate de que el rol con ID 1 existe
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Users', { username: 'admin' }, {});
+  }
+};
