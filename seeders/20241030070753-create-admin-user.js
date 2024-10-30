c577ccb2df92bc01731da107280c6c9142212af8
FlyBenja <aldanabenjamin185@gmail.com>
2024-10-30 16:32:44 -0600

    mejorando el apartado de seeder de usuario v2

--- seeders/20241030070753-create-admin-user.js
+++ seeders/20241030070753-create-admin-user.js
@@ -9,34 +9,24 @@ module.exports = {
     );
 
     if (tableExists[0][0].to_regclass) {
-      // Asegurarse de que el rol Admin existe
+      // Asegurarse de que el rol con role_id = 1 existe
       const [results] = await queryInterface.sequelize.query(
-        "SELECT * FROM Roles WHERE nombreRole = 'Admin'"
+        "SELECT * FROM Roles WHERE id = 1"
       );
 
-      // Solo insertar el usuario si el rol Admin existe
+      // Solo insertar el usuario si el rol con role_id = 1 existe
       if (results.length > 0) {
         const hashedPassword = await bcrypt.hash('admin', 10); // Hashea la contraseña
 
-        const newUser = await queryInterface.bulkInsert('Users', [{
+        await queryInterface.bulkInsert('Users', [{
           username: 'admin',
           password: hashedPassword,
-          roleId: results[0].id, // Usar el ID del rol encontrado
+          roleId: 1, // Asignar role_id = 1 fijo
           createdAt: new Date(),
           updatedAt: new Date()
-        }], { returning: true });
-
-        // Crear un pasajero relacionado al nuevo usuario
-        if (newUser.length > 0) {
-          const userId = newUser[0].id; // Obtener el ID del usuario recién creado
-          await queryInterface.bulkInsert('Pasajeros', [{
-            user_id: userId,
-            createdAt: new Date(),
-            updatedAt: new Date()
-          }]);
-        }
+        }]);
       } else {
-        console.error("El rol 'Admin' no existe. Asegúrate de que la migración de roles se haya ejecutado correctamente.");
+        console.warn("El rol con role_id = 1 no existe. No se creará el usuario.");
       }
     } else {
       console.warn("La tabla 'Roles' no existe. Asegúrate de ejecutar las migraciones primero.");
@@ -44,16 +34,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    // Eliminar el pasajero asociado al usuario
-    const [userResults] = await queryInterface.sequelize.query(
-      "SELECT id FROM Users WHERE username = 'admin'"
-    );
-
-    if (userResults.length > 0) {
-      const userId = userResults[0].id;
-      await queryInterface.bulkDelete('Pasajeros', { user_id: userId }, {});
-    }
-
     // Eliminar el usuario
     await queryInterface.bulkDelete('Users', { username: 'admin' }, {});
   }
