f80a344a7038209a54718909d23c093f2fe4b725
FlyBenja <aldanabenjamin185@gmail.com>
2024-10-31 16:02:27 -0600

    colocando el drop de tablas correctamente

--- migrations/20241029000000-drop-tables.js
+++ migrations/20241029000000-drop-tables.js
@@ -1,12 +1,16 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-      
+  async up(queryInterface, Sequelize) {
+    // Esto puede quedar vacío o contener operaciones adicionales
   },
 
-  down: async (queryInterface, Sequelize) => {
-    // No implementamos el código para volver a crear las tablas.
-    // Sin embargo, podrías hacerlo si lo necesitas.
+  async down(queryInterface, Sequelize) {
+    // Elimina primero las tablas que tienen claves foráneas
+    await queryInterface.dropTable('DatosVuelos');
+    await queryInterface.dropTable('Reservas');
+    await queryInterface.dropTable('ClaseVuelos');
+    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('Roles');
   },
 };
