1aa82f7518f5553def0f44fbbd04f226232b0e89
FlyBenja <aldanabenjamin185@gmail.com>
2024-10-31 15:57:16 -0600

    quitando el drop de tablas correctamente

--- migrations/20241029000000-drop-tables.js
+++ migrations/20241029000000-drop-tables.js
@@ -2,10 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('DatosVuelo');
-    await queryInterface.dropTable('Reservas');
-    await queryInterface.dropTable('Users');
-    await queryInterface.dropTable('Roles');
+      
   },
 
   down: async (queryInterface, Sequelize) => {
