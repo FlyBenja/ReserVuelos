381a1cc19123cd1dfd8f844aec2609b0ff85dc1b
FlyBenja <aldanabenjamin185@gmail.com>
2024-10-31 01:55:56 -0600

    Subiendo seder user

--- seeders/20241030070753-create-admin-user.js
+++ seeders/20241030070753-create-admin-user.js
@@ -6,25 +6,15 @@ module.exports = {
     // Hashear la contraseña
     const hashedPassword = await bcrypt.hash('admin', 10); // Hashea la contraseña 'admin'
 
-    // Verificar si el usuario 'admin' ya existe
-    const [results] = await queryInterface.sequelize.query(
-      "SELECT * FROM Users WHERE username = 'admin'"
-    );
+    // Insertar el usuario admin con roleId = 1
+    await queryInterface.bulkInsert('Users', [{
+      username: 'admin',
+      password: hashedPassword,
+      roleId: 1, // Asignar roleId = 1 fijo
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
 
-    // Solo insertar si no existe
-    if (results.length === 0) {
-      // Insertar el usuario admin con roleId = 1
-      await queryInterface.bulkInsert('Users', [{
-        username: 'admin',
-        password: hashedPassword,
-        roleId: 1, // Asignar roleId = 1 fijo
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }]);
-
-      console.log("Usuario 'admin' creado exitosamente.");
-    } else {
-      console.log("El usuario 'admin' ya existe en la tabla 'Users'. No se necesita crear uno nuevo.");
-    }
-  },
+    console.log("Usuario 'admin' creado exitosamente.");
+  }
 };
